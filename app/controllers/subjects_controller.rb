9e5deb85cb89654daf0ca1576e891e25f5d6b189
bhagathreddy123 <katta_reddy04@yahoo.com>
2021-02-14 03:54:45 -0800

    error partial and custom helper implementation

--- app/controllers/subjects_controller.rb
+++ app/controllers/subjects_controller.rb
@@ -4,12 +4,50 @@ class SubjectsController < ApplicationController
   end
 
   def new
+  	@subject = Subject.new
+  end
+
+  def create
+  	@subject = Subject.new(subject_params)
+    # Save the object
+    if @subject.save
+      # If save succeeds, redirect to the index action
+      flash[:notice] = 'Subject created'
+      redirect_to(subjects_path)
+    else
+      # If save fails, redisplay the form so user can fix problems
+      render('new')
+    end
   end
 
   def edit
+  	@subject = Subject.find(params[:id])
+  end
+  
+  def update
+    @subject = Subject.find(params[:id])
+    if @subject.update(subject_params)
+      flash[:notice] = 'Subject updated'
+      redirect_to(subject_path(@subject))
+    else
+      render('edit')
+    end
   end
 
   def show
   	@subject = Subject.find(params[:id])
   end
+
+  def destroy
+  	@subject = Subject.find(params[:id])
+  	if @subject.destroy
+  		redirect_to subjects_path
+  	end
+  end
+
+  private
+
+    def subject_params
+      params.require(:subject).permit(:name, :visible, :position)
+    end
 end
